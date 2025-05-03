c9d385e538706d466a666cd23fa56e1333e791a3
AirPheonixSaksham <166398246+AirPheonixSaksham@users.noreply.github.com>
2025-05-03 14:16:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,10 +36,11 @@ from vars import API_ID, API_HASH, BOT_TOKEN
 
 # watermark_text = ""
 
- bot = Client("bot",
-              bot_token=os.environ.get("BOT_TOKEN"),
-              api_id=int(os.environ.get("API_ID")),
-              api_hash=os.environ.get("API_HASH"))
+bot = Client(
+    "bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN)
 
 
 
