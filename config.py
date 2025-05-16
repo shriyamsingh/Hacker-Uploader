50db71acf200a3e582f5dc986bb183a13beea005
shriyamsingh <farziiasinstitute@gmail.com>
2025-05-16 23:30:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-api_id =  28829496
-api_hash  = '8094341dbe5bd50090de8b12a01d04b8'
-bot_token =  '8121411999:AAHj-WZRHzzXrVFcohrAHuKrqVZ85E3GYqk'
-LOG =  -100253268913
+api_id =  20942443
+api_hash  = '52c5ee66fed9e3e1afe0117a7f53bb75'
+bot_token =  '7213057490:AAFz-DYjBJ9dWZaZjQlbY_-nPlYuQSf7yLg'
+LOG =  -1002589441691
 
